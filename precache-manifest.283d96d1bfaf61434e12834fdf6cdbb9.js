7c1f93be15a7d74a3210f63a769aa75d463a4e8f
Jason Welsh <jasonbwelsh@gmail.com>
2020-04-07 12:34:02 -0400

    Updates

---
+++ precache-manifest.283d96d1bfaf61434e12834fdf6cdbb9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e276c885ef3a3734cb08e6a5a9a5777",
+    "url": "/jasonbwelsh.github.io/index.html"
+  },
+  {
+    "revision": "256d839a078315d28bd5",
+    "url": "/jasonbwelsh.github.io/static/css/main.10a82623.chunk.css"
+  },
+  {
+    "revision": "3cffa515270b369521e2",
+    "url": "/jasonbwelsh.github.io/static/js/2.9b9eda63.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/jasonbwelsh.github.io/static/js/2.9b9eda63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "256d839a078315d28bd5",
+    "url": "/jasonbwelsh.github.io/static/js/main.40137234.chunk.js"
+  },
+  {
+    "revision": "011cb33c6e789d9378e4",
+    "url": "/jasonbwelsh.github.io/static/js/runtime-main.5a5fb1cc.js"
+  },
+  {
+    "revision": "bc732b22ae807ed09633c87fcd546075",
+    "url": "/jasonbwelsh.github.io/static/media/ashleyMadisonLogo.bc732b22.jpg"
+  },
+  {
+    "revision": "1ce7be5ec325b8620c2e90a2724a8810",
+    "url": "/jasonbwelsh.github.io/static/media/cvs-logo.1ce7be5e.jpeg"
+  },
+  {
+    "revision": "ccd86edee6ecc38054d8ee4e1fa4f9d8",
+    "url": "/jasonbwelsh.github.io/static/media/peoples.ccd86ede.jpg"
+  }
+]);
